ed009cd1fb0b8681888247874d6a97abea374fe1
Ajay <ajaynimmala18@gmail.com>
2025-12-22 19:15:37 +0530

    Basic CRUD operations (Create,Read,Update,Delete) using hibernate

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,18 +9,21 @@ public class Main {
     public static void main(String[] args) {
 
         Student s1 = new Student();
-        s1.setsName("Ganesh");
-        s1.setRollNo(20);
+        s1.setsName("ramesh");
+        s1.setRollNo(23);
         s1.setAge(20);
-
-        Configuration cfg = new Configuration();
-        cfg.addAnnotatedClass(org.example.Student.class);
-        cfg.configure();
-        SessionFactory sf = cfg.buildSessionFactory();
+//        Configuration cfg = new Configuration();
+//        cfg.addAnnotatedClass(org.example.Student.class);
+//        cfg.configure();
+        SessionFactory sf = new Configuration()
+                .addAnnotatedClass(org.example.Student.class)
+                .configure()
+                .buildSessionFactory();
         Session session = sf.openSession();
-        Transaction transaction = session.beginTransaction();
-        session.persist(s1);
-        transaction.commit();
-        System.out.println(s1);
+        Student s2 = null;
+        s2 = session.find(Student.class,21);
+        session.close();
+        sf.close();
+        System.out.println(s2);
     }
 }
